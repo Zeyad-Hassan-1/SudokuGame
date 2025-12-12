fba604d48f3e5b4a02ea096b604549debb53529c
zeyad-hassan-1 <studying.zezo@gmail.com>
2025-12-12 21:00:32 +0200

    Add placeholder for solver classes

---
+++ SudokuVerifier/src/com/mycompany/app/controllers/services/PermutationIterator.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.controllers.services;
+
+/**
+ * ITERATOR PATTERN: Generates permutations on-demand.
+ * 
+ * @author
+ */
+public class PermutationIterator {
+
+}
---
+++ SudokuVerifier/src/com/mycompany/app/controllers/services/SudokuSolver.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.controllers.services;
+
+/**
+ * Sudoku Solver using Iterator and Flyweight design patterns with parallelism.
+ * 
+ * @author
+ */
+public class SudokuSolver {
+
+}
