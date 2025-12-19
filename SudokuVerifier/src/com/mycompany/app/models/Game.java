ac596f0e996a5eb332e5424f4d5232ab269ae549
Nour alden mohamed <nouraldenmohamed35@gmail.com>
2025-12-19 14:44:06 +0200

    create game

--- SudokuVerifier/src/com/mycompany/app/models/Game.java
+++ SudokuVerifier/src/com/mycompany/app/models/Game.java
@@ -12,11 +12,27 @@ package com.mycompany.app.models;
  */
 public class Game {
    int[][] board;
+   private DifficultyEnum difficulty;
 
-   public Game(int[][] board) {
+   public Game(int[][] board, DifficultyEnum difficulty) {
       // IMPORTANT: DON'T COPY THE BOARD BY VALUE
       // USE REFERENCES
       this.board = board;
+      this.difficulty = difficulty;
    }
    // Add methods and attributes if needed
+
+   public int[][] getBoard() {
+      return board;
+   }
+
+   public void setBoard(int[][] board) {
+      this.board = board;
+   }
+
+ 
+
+ 
+
+}
 }
